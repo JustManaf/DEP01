d0175b1758f6b8a6149be556ddf8ffd8d6bb285c
JustManaf <manafalbaqshi@gmail.com>
2023-12-04 09:19:14 +0300

    Fixing

--- public/application.js
+++ public/application.js
@@ -1,1 +1,13 @@
+const express = require('express'); 
+const http = require('http'); 
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(__dirname + '/public'));
+
 console.log("Hello World");
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000'); 
+});
